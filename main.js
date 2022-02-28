c6a9c85e8377f0a24acfac8648b8a6ae257830a0
Bharti277 <bhartideependra277@gmail.com>
2022-02-28 13:37:52 +0530

    random number applied

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var bgColorBody = document.getElementById("main");
 button.addEventListener("click", changeColor);
 
 function changeColor(e) {
-  for (let i = 0; i < 255; i++) {
-    bgColorBody.style.backgroundColor = `rgba(${Math.random() * 255}, ${
-      Math.random() * 255
-    }, ${Math.random() * 255})`;
+    const num1 = Math.floor(Math.random() * 255);
+    const num2 = Math.floor(Math.random() * 255);
+    const num3 = Math.floor(Math.random() * 255);
+    bgColorBody.style.backgroundColor = `rgb(${num1}, ${num2}, ${num3})`
     textColor.textContent = bgColorBody.style.backgroundColor;
-  }
+  
 }
